dc043a668cb4a946fbcdcf1cf8e6e599424628f9
jeff123 <sanchit_sharma1@outlook.com>
2018-12-01 20:14:32 -0800

    tinyupdate

--- src/client/ButtonActionListener.java
+++ src/client/ButtonActionListener.java
@@ -11,6 +11,6 @@ public class ButtonActionListener implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        client.showMessage(client.getName());
+        client.showMessage("\n"+client.getName());
     }
 }
--- src/client/ClientTest.java
+++ src/client/ClientTest.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class ClientTest {
 public static void main(String args[]){
     Client c;
-    c = new Client("127.0.0.1",  "nickname",  "name" , "contact", "ethnicity", "gender", "age" );
+    c = new Client("127.0.0.1",  "nickname",  "Christopher" , "contact", "ethnicity", "gender", "age" );
     c.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     c.startRunning();
 
